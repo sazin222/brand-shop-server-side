00e99093201e3c649fdf6af902ca51720fdcd482
sazin222 <mahadiislamsazin.com>
2023-10-21 13:46:23 +0600

    added the add band get and post

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
 const cors = require('cors');
 require('dotenv').config()
-const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+
+
 const port= process.env.PORT || 5000 
 
 
@@ -10,7 +13,6 @@ const port= process.env.PORT || 5000
 app.use(cors());
 app.use(express.json()) 
   
-
 
 
 const uri = `mongodb+srv://${process.env.DB_USERS}:${process.env.DB_PASS}@cluster0.na448cw.mongodb.net/?retryWrites=true&w=majority`;
@@ -27,11 +29,12 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     
     const userCollection = client.db('productDB').collection('product')
-
-
+    const brandCollection= client.db('brandDB').collection('brand') 
+    
+    
     app.post('/products', async(req,res)=>{
       const product = req.body 
       console.log(product); 
@@ -39,11 +42,11 @@ async function run() {
       res.send(result)
    }) 
 
-   app.get('/product', async (req,res)=>{
-    const coursor= userCollection.find()
-    const product = await coursor.toArray() 
-    res.send(product)
- })
+//    app.get('/product', async (req,res)=>{
+//     const coursor= userCollection.find()
+//     const product = await coursor.toArray() 
+//     res.send(product)
+//  })
 
  
  app.get('/products/:brand', async(req,res)=>{
@@ -92,18 +95,35 @@ async function run() {
   res.send(result)
 })
 
- 
- app.delete('/product/:id', async (req,res)=>{
-  const id = req.params.id
-  console.log({id});
-  const query ={_id: new ObjectId(id)}
-  const result = await userCollection.deleteOne(query)
+
+
+app.post('/brand', async(req,res)=>{
+  const brand = req.body 
+  console.log(brand); 
+  const result = await brandCollection.insertOne(brand)
   res.send(result)
 }) 
 
 
+app.get('/brand', async(req,res)=>{
+  const coursor= brandCollection.find()
+  const result = await coursor.toArray()
+  res.send(result)
+
+}) 
+
+app.delete('/brands/:id', async (req,res)=>{
+const id = req.params.id
+console.log({id});
+const query ={_id: id}
+const result = await brandCollection.deleteOne(query)
+res.send(result)
+}) 
+
+
+
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
